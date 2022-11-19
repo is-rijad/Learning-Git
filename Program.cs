f467447a5d78b918537134a343eabf23637a46c9
isrijad <isrijad@gmail.com>
2022-11-19 10:50:25 +0100

    I'm the second

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ namespace MyProgram
         public static void Main(string[] args) {
             System.Console.WriteLine("I'm learning git!");
             System.Console.WriteLine("This is the second commit!");
+            System.Console.WriteLine("I'm the second person!");
         }
     }
 }
