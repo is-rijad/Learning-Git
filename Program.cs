e327a6049e1841444c20240592e2e38664dc9f50
isrijad <isrijad@gmail.com>
2022-11-19 10:29:20 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace MyProgram
+{
+    public class Program {
+        public static void Main(string[] args) {
+            System.Console.WriteLine("I'm learning git!");
+        }
+    }
+}
